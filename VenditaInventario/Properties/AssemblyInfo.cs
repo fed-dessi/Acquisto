bc6d52023afc7ba21aa291865e5f2680c71a1e9b
Federico Dessì <fed-dessi@users.noreply.github.com>
2022-07-01 14:24:08 +0200

    bumped the version number

--- VenditaInventario/Properties/AssemblyInfo.cs
+++ VenditaInventario/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Oberdan 15")]
 [assembly: AssemblyProduct("Vendita")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.8.6")]
-[assembly: AssemblyFileVersion("1.0.8.6")]
+[assembly: AssemblyVersion("1.0.8.7")]
+[assembly: AssemblyFileVersion("1.0.8.7")]
 
 // Configure log4net using the .config file
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
